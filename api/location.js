e8772322d80f905024f32f7e00996b666bbdd622
Alfren Alvaran <alvaranalfren1@gmail.com>
2025-09-30 17:51:33 +0800

    change

---
+++ api/location.js
@@ -0,0 +1,26 @@
+import requestIp from "request-ip";
+
+export default async function handler(req, res) {
+  try {
+    let ip = requestIp.getClientIp(req) || "";
+    ip = ip.replace("::ffff:", "");
+
+    const url =
+      ip === "127.0.0.1"
+        ? "https://ipapi.co/json/"
+        : `https://ipapi.co/${ip}/json/`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.status(200).json({
+      ip,
+      latitude: data.latitude,
+      longitude: data.longitude,
+      city: data.city,
+      country: data.country_name,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch location" });
+  }
+}
